f63648ac1544ba1539fca64499ce942723e222d0
Donestonke <donestonke@gmail.com>
2026-03-22 01:47:49 +0700

    Create This_ex1.cpp

---
+++ BaiThucHanh/Bai 3/This_ex1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Circle2.h"
+using namespace std;
+
+int main(void){
+    // Create an object
+    Circle2 obj1;
+
+    obj1.setRadius(5);
+    obj1.printPointer();
+
+    system("pause");
+    return 0;
+}
