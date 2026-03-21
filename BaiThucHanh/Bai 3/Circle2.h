98066c87373b9ff9bb127146e34c26b71f3a6fb7
Donestonke <donestonke@gmail.com>
2026-03-22 01:47:21 +0700

    Create Circle2.h

---
+++ BaiThucHanh/Bai 3/Circle2.h
@@ -0,0 +1,14 @@
+class Circle2 {
+    // member data
+    private:
+        double r;
+    // member functions
+    public:
+        Circle2();
+        Circle2(double r);
+        void setRadius(double r);
+        double getRadius(void);
+        double calcArea(void);
+        double calcCirc(void);
+        void printPointer(void);
+}; // end Circle2 class
