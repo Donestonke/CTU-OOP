396085d6a2aa987a6327f55c723d4d3473663a98
Donestonke <donestonke@gmail.com>
2026-03-22 01:43:51 +0700

    Create Des.h

---
+++ BaiThucHanh/Bai 2c/Des.h
@@ -0,0 +1,16 @@
+#ifndef DES_H_
+#define DES_H_
+// Des class declaration
+class Des{
+    // member data
+    private:
+        double var1;
+    // member functions
+    public:
+        Des(void);// constructor
+        ~Des(void);// destructor
+        void setVar1(double v1);
+        double getVar1(void);
+};
+
+#endif /* DES_H_ */
