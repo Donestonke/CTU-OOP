099172085511181cb37be68d16b3a716448a891b
Donestonke <donestonke@gmail.com>
2026-03-22 01:37:24 +0700

    Add Cons class declaration in cons.h

---
+++ BaiThucHanh/Bai 2/cons.h
@@ -0,0 +1,14 @@
+    #ifndef CONS_H_
+    #define CONS_H_
+    // Cons class declaration
+    class Cons{
+    // member data
+    private:
+        double var1;
+    // member functions
+    public:
+        Cons(void);
+        void setVar1(double v1);
+        double getVar1(void);
+    };
+    #endif /* CONS_H_ */
