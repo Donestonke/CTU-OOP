ad443ac29d8acfb35928e80fbf2ccb18055fc74b
Donestonke <donestonke@gmail.com>
2026-03-22 08:49:17 +0700

    Update Account.h

--- BaiThucHanh/Bai 6/Account.h
+++ BaiThucHanh/Bai 6/Account.h
@@ -1,1 +1,23 @@
+#ifndef ACCOUNT_H_
+#define ACCOUNT_H_
 
+class Account {
+private:
+    double savings;
+    double checking;
+public:
+    Account(void);
+    Account(double s, double c);
+    void setSavings(double s);
+    void setChecking(double c);
+    double getSavings(void);
+    double getChecking(void);
+    double calcBalance(void) const;
+    Account operator+(const Account & rhs);
+    Account operator-(const Account & rhs);
+    bool operator==(const Account & rhs);
+    bool operator<(const Account & rhs);
+    bool operator>(const Account & rhs);
+};
+
+#endif /* ACCOUNT_H_ */
